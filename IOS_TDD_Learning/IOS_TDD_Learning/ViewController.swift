cdbd7ba1173bac3d58c3631dc145ef0d30ff8b61
Wenqi Yuan <wqyuan@thoughtworks.com>
2018-07-17 00:25:09 +0800

    [Wenqi] build project

---
+++ IOS_TDD_Learning/IOS_TDD_Learning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS_TDD_Learning
+//
+//  Created by Wenqi Yuan  on 17/07/2018.
+//  Copyright © 2018 Wenqi Yuan . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ IOS_TDD_Learning/IOS_TDD_LearningTests/IOS_TDD_LearningTests.swift
@@ -0,0 +1,36 @@
+//
+//  IOS_TDD_LearningTests.swift
+//  IOS_TDD_LearningTests
+//
+//  Created by Wenqi Yuan  on 17/07/2018.
+//  Copyright © 2018 Wenqi Yuan . All rights reserved.
+//
+
+import XCTest
+@testable import IOS_TDD_Learning
+
+class IOS_TDD_LearningTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
