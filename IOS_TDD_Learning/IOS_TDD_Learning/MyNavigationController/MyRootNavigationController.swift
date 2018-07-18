952294412097ea45f7d722600397ffb2669aba62
Wenqi Yuan <wqyuan@thoughtworks.com>
2018-07-19 00:46:32 +0800

    [Wenqi]  learning Typhoom

---
+++ IOS_TDD_Learning/IOS_TDD_Learning/MyNavigationController/MyRootNavigationController.swift
@@ -0,0 +1,13 @@
+//
+// Created by Wenqi Yuan  on 18/07/2018.
+// Copyright (c) 2018 Wenqi Yuan . All rights reserved.
+//
+
+import Foundation
+import UIKit
+class MyRootNavigationController:UINavigationController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationBar.backgroundColor = .red
+    }
+}
---
+++ IOS_TDD_Learning/IOS_TDD_Learning/TestView.swift
@@ -0,0 +1,13 @@
+//
+// Created by Wenqi Yuan  on 19/07/2018.
+// Copyright (c) 2018 Wenqi Yuan . All rights reserved.
+//
+
+import Foundation
+import UIKit
+class TestView: UIView{
+    override init(frame: frame){
+        super.init(frame: frame)
+        self.backgroundColor = .cyan
+    }
+}
